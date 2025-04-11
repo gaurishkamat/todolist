e4d65a312cdd25e0b394bec1a1d25bdaa4b30bef
Gaurish <gaurishkamat23@gmail.com>
2025-04-11 22:21:20 +0530

    adding auth files

---
+++ src/main/java/com/trello/todolist/dtos/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.trello.todolist.dtos;
+
+public class AuthResponse {
+    private String token;
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+    public String getToken() {
+        return token;
+    }
+}
