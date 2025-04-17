c603d3c09449c302a7be0e462ed9ac00406544ac
Gaurish <gaurishkamat23@gmail.com>
2025-04-17 23:06:20 +0530

    adding auth files

---
+++ src/main/java/com/trello/todolist/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.trello.todolist.controller;
+
+import com.trello.todolist.dtos.AuthRequest;
+import com.trello.todolist.dtos.AuthResponse;
+import com.trello.todolist.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest, authRequest.getPassword())
+            );
+            String token = jwtUtil.generateToken(authRequest.getUsername());
+            return ResponseEntity.ok(new AuthResponse(token));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+    }
+}
