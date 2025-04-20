8f7f990a79ad1605d7daad986626f80dd075c7b6
Gaurish <gaurishkamat23@gmail.com>
2025-04-20 22:56:12 +0530

    adding jwt filter

--- src/main/java/com/trello/todolist/controller/AuthController.java
+++ src/main/java/com/trello/todolist/controller/AuthController.java
@@ -9,10 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -23,6 +20,12 @@ public class AuthController {
 
     @Autowired
     private JwtUtil jwtUtil;
+
+    @GetMapping("/greet")
+    public String greet()
+    {
+            return "Welcome to ToDo's!!";
+    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
