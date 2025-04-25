5b45cc5fc5a9bf1b1b0260885a9df4290960d052
Gaurish <gaurishkamat23@gmail.com>
2025-04-25 21:23:42 +0530

    adding item model

---
+++ src/main/java/com/trello/todolist/model/ListItem.java
@@ -0,0 +1,73 @@
+package com.trello.todolist.model;
+
+import java.util.Date;
+
+public class ListItem {
+    private int Id;
+    private String title;
+    private String description;
+    private String dateCreated;
+    private String status;
+
+    public ListItem() {
+    }
+
+    public ListItem(int id, String title, String description, String dateCreated, String status) {
+        Id = id;
+        this.title = title;
+        this.description = description;
+        this.dateCreated = dateCreated;
+        this.status = status;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "ListItem{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dateCreated='" + dateCreated + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
