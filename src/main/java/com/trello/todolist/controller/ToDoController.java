f231ab8915ea3abef5b552238596632c41c29894
Gaurish <gaurishkamat23@gmail.com>
2025-04-26 21:02:32 +0530

    adding todos get api

--- src/main/java/com/trello/todolist/controller/ToDoController.java
+++ src/main/java/com/trello/todolist/controller/ToDoController.java
@@ -1,8 +1,12 @@
 package com.trello.todolist.controller;
 
+import com.trello.todolist.model.ListItem;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -12,5 +16,10 @@ public class ToDoController {
     public String greet()
     {
         return "Welcome to ToDo's!!";
+    }
+
+    @GetMapping("/todos")
+    public List<ListItem> getList(){
+        return List.of(new ListItem(1, "Wash Hands", "Wash your hands", "10/12/2024", "pending"));
     }
 }
