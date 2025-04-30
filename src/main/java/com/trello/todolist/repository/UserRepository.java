46a77f12ec6dc0d919b9559d417a7c6565c1403a
Gaurish <gaurishkamat23@gmail.com>
2025-04-30 12:51:40 +0530

    user repository issue fixed

--- src/main/java/com/trello/todolist/repository/UserRepository.java
+++ src/main/java/com/trello/todolist/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.trello.todolist.repository;
 
+import com.trello.todolist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
