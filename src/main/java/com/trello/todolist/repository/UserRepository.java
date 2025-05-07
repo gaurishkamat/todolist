f471be740e5c53b2fa06405defd3b880e0754861
Gaurish <gaurishkamat23@gmail.com>
2025-05-07 23:13:26 +0530

    UserDetailsService changes

--- src/main/java/com/trello/todolist/repository/UserRepository.java
+++ src/main/java/com/trello/todolist/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.trello.todolist.repository;
 
 import com.trello.todolist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/trello/todolist/service/MyUserDetailsService.java
+++ src/main/java/com/trello/todolist/service/MyUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.trello.todolist.service;
 
-import org.springframework.security.core.userdetails.User;
+import com.trello.todolist.model.User;
+import com.trello.todolist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,17 +10,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) {
         // Replace with DB call
-        if ("admin".equals(username)) {
-            return new User("admin", new BCryptPasswordEncoder().encode("password"),
-                    new ArrayList<>());
-        }
-        throw new UsernameNotFoundException("User not found");
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(), // should be already encoded
+                Collections.emptyList() // authorities/roles can be added here
+        );
     }
 }
