407a99a007e1989d0047c831e398038707e60dd0
Gaurish <gaurishkamat23@gmail.com>
2025-05-13 20:15:45 +0530

    adding swagger

--- src/main/java/com/trello/todolist/utils/SecurityConfig.java
+++ src/main/java/com/trello/todolist/utils/SecurityConfig.java
@@ -30,6 +30,13 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/**").permitAll()
+                .requestMatchers(
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**",
+                        "/v3/api-docs",
+                        "/webjars/**"
+                ).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
